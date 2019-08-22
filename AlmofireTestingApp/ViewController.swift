f31e185a7986436643cebd9dea2a4839ca6fc668
Shreeshail <shreeshaila.v@hashedin.com>
2019-08-22 11:06:09 +0530

    Initial Commit

---
+++ AlmofireTestingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlmofireTestingApp
+//
+//  Created by hasher on 22/08/19.
+//  Copyright © 2019 hasher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
